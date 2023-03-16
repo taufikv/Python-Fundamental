04a727d7c3bf088ae1d5f13ac17c28a8ab00bf32
Taufik Budi Santosa <taufikbudis@gmail.com>
2023-03-16 09:35:23 +0800

    Perulangan dengan while

--- Python Fundamental - Perulangan_dengan_while.py
+++ Python Fundamental - Perulangan_dengan_while.py
@@ -3,7 +3,16 @@ Program perulangan membaca buku dengan while
 """
 
 jumlah_buku = 10
+print(f'Jumlah buku {jumlah_buku}')
 print('Ibu berkata, "Baca semua buku"')
+print('Anak menjawab, "OK"')
 
 jumlah_buku_yang_sudah_dibaca = 0
+print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
 
+while jumlah_buku_yang_sudah_dibaca < jumlah_buku:
+    jumlah_buku_yang_sudah_dibaca = jumlah_buku_yang_sudah_dibaca + 1
+    print(f'Buku ke-{jumlah_buku_yang_sudah_dibaca} sudah dibaca')
+
+print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
+print('Lapor ke ibu semua buku sudah dibaca')
